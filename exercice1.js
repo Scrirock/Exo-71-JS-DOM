dd89a31879509f9bbeb3a30533e6afcfe815b75f
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-04 09:39:22 +0100

    ui

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,14 @@
-let monDiv = .createElement('div');
-
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 document.getElementById('viewport').appendChild(monDiv);
+
+let secondDiv = document.createElement('div');
+secondDiv.style.backgroundColor = "red";
+secondDiv.style.width = "100px";
+secondDiv.style.height = "100px";
+document.getElementById('viewport').append(secondDiv);
+
+document.getElementById('aSupprimer').remove();
 
 
 
